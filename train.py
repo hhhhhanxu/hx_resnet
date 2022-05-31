0f076fbbc05ba7786f2dd6709871bb9834292cd8
hhhhhanxu <71363087+hhhhhanxu@users.noreply.github.com>
2022-05-31 17:02:41 +0800

    Update train.py

--- train.py
+++ train.py
@@ -19,17 +19,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 matplotlib.use('Agg')
 
-
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
-
 from dataset import langqiao_dataset
 from model import My_resnet
 from test import test
 from utils.utils import select_device,init_torch_seeds
+from utils.warmup import CosineAnnealingLRWarmup
+from utils.smooth_loss import LabelSmoothingCrossEntropy
 
 
 @logger.catch
@@ -60,10 +61,15 @@ def train(opt,device):
     if cuda: 
         model = model.cuda()
     #损失函数
-    criterion=nn.CrossEntropyLoss()
+    # criterion = LabelSmoothingCrossEntropy()  # 标签平滑
+    criterion = nn.CrossEntropyLoss()
     #优化器
     optimizer = optim.SGD(model.parameters(),lr=opt.lr,momentum=opt.momentum)
-    
+    #学习率调整
+    scheduler = CosineAnnealingLRWarmup(optimizer,T_max=100,eta_min=opt.lr/100,warmup_steps=15)
+
+    # test for every epoch 
+    best_acc = 0
     
     for epoch in range(opt.epochs):
         model.train()
@@ -78,7 +84,9 @@ def train(opt,device):
             loss.backward()
             optimizer.step()
             pbar.set_description('epoch:{}   {}/{}'.format(epoch,batch_idx+1,len(train_loader)))
+        scheduler.step()
         wandb.log({'train/loss':loss})
+        wandb.log({'train/lr':scheduler.get_last_lr()[0]})
         # test for every epoch 
         accuracy,cm_value = test(opt,device,model,test_loader,epoch)
         wandb.log({'test/accuracy':accuracy})
@@ -89,20 +97,21 @@ def train(opt,device):
         if opt.epochs - epoch < 0.1*opt.epochs:
             #最后10%的epoch上传 cm img
             wandb.log({"Confusion_matrix": [wandb.Image(str(x), caption=x.name) for x in Path(opt.save_dir).glob('*.png')]})
-
+        if accuracy > best_acc:
+            torch.save(model.state_dict(),os.path.join(opt.save_dir,'best.pth'))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='a litte effort for my resnet')
-    parser.add_argument('--data_dir',type=str,required=True)
-    parser.add_argument('--save_dir',type=str,required=True)
-    parser.add_argument('--batch_size',type=int,default=16)
+    parser.add_argument('--data_dir',type=str,default="/data_plus/dataset/my_fgvc_dataset/",required=True)
+    parser.add_argument('--save_dir',type=str,default='result/',required=True)
+    parser.add_argument('--batch_size',type=int,default=32)
     parser.add_argument('--epochs',type=int,default=100)
     parser.add_argument('--lr',type=float,default=0.01)
     parser.add_argument('--momentum',type=float,default=0.937)
     parser.add_argument('--num_class',type=int,default=3)
-    parser.add_argument('--device', default='1', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--device', default='0,1,2,3', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--use_pretrained',action='store_true',help='use pretrained resnet model')
-    parser.add_argument('--model_size', type=int, default=101, choices=[34,50,101,152],help='the size of resnet')
+    parser.add_argument('--model_size', type=int, default=50, choices=[18,34,50,101,152],help='the size of resnet')
     
     opt = parser.parse_args()
 
